3612f01ad4dafaeebee4fd361ef6d952606551a1
lucassilvavbl <lucas.silva@vonbraunlabs.com.br>
2024-09-09 22:07:01 -0300

    ajustes pequenos nas importações

--- src/main/java/br/dc/compiladores/t2_compiladores/Principal.java
+++ src/main/java/br/dc/compiladores/t2_compiladores/Principal.java
@@ -1,11 +1,11 @@
 package br.dc.compiladores.t2_compiladores;
 
-import java.io.IOException;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
-import java.io.PrintWriter;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Principal {
     public static void main(String args[]) throws IOException {
